4875ccafdded8b793384c9e7ea6044454271e673
Sam Oakley <sam@blork.co.uk>
2012-02-23 00:45:30 +0000

    Tabs to spaces.

--- codebase.py
+++ codebase.py
@@ -20,46 +20,46 @@ xml_template = """
 """
 
 def main():
-	try:
-		userdata = json.load(open("userdata.json"))
-		username = userdata["username"]
-		key = userdata["key"]
-		if username == "" or key == "":
-			raise
-	except:
-		sys.exit("Misconfigured. Edit userdata.json.")
+    try:
+        userdata = json.load(open("userdata.json"))
+        username = userdata["username"]
+        key = userdata["key"]
+        if username == "" or key == "":
+            raise
+    except:
+        sys.exit("Misconfigured. Edit userdata.json.")
 
-	while True:
-		task_name = raw_input("Enter a task summary: ")
+    while True:
+        task_name = raw_input("Enter a task summary: ")
 
-		start = time.time()
-		raw_input("Press any key when complete.")
+        start = time.time()
+        raw_input("Press any key when complete.")
 
-		time_spent = int(math.ceil((time.time() - start)/60))
+        time_spent = int(math.ceil((time.time() - start)/60))
 
-		print "You spend {0} minutes on that task.".format(time_spent)
-		upload = True if raw_input("Upload to Codebase? Y/N ").lower() == "y" else False
+        print "You spend {0} minutes on that task.".format(time_spent)
+        upload = True if raw_input("Upload to Codebase? Y/N ").lower() == "y" else False
 
-		if upload:
-			xml = xml_template.format(task_name, time_spent)
+        if upload:
+            xml = xml_template.format(task_name, time_spent)
 
-			req = urllib2.Request(url=sessions_url, data=xml)
+            req = urllib2.Request(url=sessions_url, data=xml)
 
-			base64string = base64.encodestring('%s:%s' % (username, key)).replace('\n', '')
-			authheader =  "Basic %s" % base64string
+            base64string = base64.encodestring('%s:%s' % (username, key)).replace('\n', '')
+            authheader =  "Basic %s" % base64string
 
-			req.add_header("Authorization", authheader)
-			req.add_header("Content-type", "application/xml")
-			req.add_header("Accept", "application/xml")
+            req.add_header("Authorization", authheader)
+            req.add_header("Content-type", "application/xml")
+            req.add_header("Accept", "application/xml")
 
-			try:
-			    f = urllib2.urlopen(req)
-			    print "Uploaded."
-			except IOError, e:
-			    if hasattr(e, 'code'):
-			    	print "There was an error."
-		        	print BaseHTTPServer.BaseHTTPRequestHandler.responses[e.code]
+            try:
+                f = urllib2.urlopen(req)
+                print "Uploaded."
+            except IOError, e:
+                if hasattr(e, 'code'):
+                    print "There was an error."
+                    print BaseHTTPServer.BaseHTTPRequestHandler.responses[e.code]
 
 
 if __name__ == "__main__":
-	main()
+    main()
